c20532262398af09df34a24e7777a1e70b229d64
chmanjeet143-lang <chmanjeet143@gmail.com>
2026-08-15 23:25:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
+from flask import Flask
+from threading import Thread
 import os
-import discord
 from discord.ext import commands
+import discord
 
-# Intents setUp
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is online!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.presences = True
 
-bot = commands.Bot(command_prefix="&", intents=intents)
+bot = commands.Bot(command_prefix='&', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'Logged in as {bot.user}')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
 
-# Token from Environment Variable
-token = os.getenv('DISCORD_TOKEN')
-bot.run(token)
+@bot.command()
+async def say(ctx, *, message: str):
+    await ctx.message.delete()
+    await ctx.send(message)
+
+keep_alive()
+
+bot.run(os.getenv('DISCORD_TOKEN'))
